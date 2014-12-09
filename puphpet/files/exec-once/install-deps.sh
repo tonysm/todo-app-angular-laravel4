debcc84273773fa0269fc25d63d353a52301a29a
Tony Messias <tonyzrp@gmail.com>
2014-12-08 23:35:10 -0300

    adding a few more steps

--- puphpet/files/exec-once/install-deps.sh
+++ puphpet/files/exec-once/install-deps.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo apt-get install -y php5-sqlite php5-mysql
+sudo apt-get install -y php5-sqlite php5-mysql curl
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /bin/composer
+sudo chown vagrant:vagrant /bin/composer
 
 sudo su vagrant
 cd ~/todo && composer install
