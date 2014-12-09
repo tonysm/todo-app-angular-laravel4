26c71a236d737e2f6273f8b38c2664b905bbd5da
Tony Messias <tonyzrp@gmail.com>
2014-12-08 22:04:14 -0300

    fixing cache release

--- src/Listeners/TodoCacheCleanerListener.php
+++ src/Listeners/TodoCacheCleanerListener.php
@@ -2,25 +2,11 @@
 
 namespace App\Listeners;
 
-use Illuminate\Cache\CacheManager;
-
 class TodoCacheCleanerListener
 {
-    /**
-     * @var \Illuminate\Cache\CacheManager
-     */
-    private $cacheManager;
-
-    /**
-     * @param CacheManager $cacheManager
-     */
-    function __construct(CacheManager $cacheManager)
-    {
-        $this->cacheManager = $cacheManager;
-    }
 
     public function clear($event)
     {
-        $this->cacheManager->forget("todos_task_cache");
+        \Cache::forget("todos_task_cache");
     }
 } 
